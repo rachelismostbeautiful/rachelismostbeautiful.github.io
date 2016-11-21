a046a876558900c1f90b205cc237216c49bb773f
b1anker <narutowontstop@gmail.com>
2016-11-21 22:44:17 +0800

    add add function and adjust html structure

--- resume/js/main.js
+++ resume/js/main.js
@@ -1,43 +1,42 @@
-window.onload = function(){
-	let oPs = document.querySelectorAll("p");
-	for(let i = 0; i < oPs.length; i++){
-		oPs[i].contentEditable = true;
-	}
-	let names = document.querySelectorAll(".name");
-	for(let i = 0; i < names.length; i++){
-		names[i].contentEditable = true;
-	}
+$(document).ready(function() {
+  $("p").attr("contentEditable", "true");
 
-	
+  removeItem(true);
+  $(".head").css({
+    position: "relative"
+  }).append(`<i class='icon-plus add'></i>`);
 
-	let deletes = document.querySelectorAll(".describe p");
-	for(let i = 0; i < deletes.length; i++){
-		let item = document.createElement("i");
-		item.className = "icon-remove remove";
-		deletes[i].appendChild(item);
-	}
+  $(".add").on("click", function(e) {
+    e = e || window.event;
+    if ($(this).parent()) {
+      $(this).parent().next().append(`<p class="newP" style="position:relative" contenteditable="true">Hello</p>`);
+    }
+    removeItem(false);
+  });
 
-	let removes = document.querySelectorAll(".remove");
-	for(let i = 0; i < deletes.length; i++){
-		removes[i].onclick = function(){
-			this.parentNode.parentNode.removeChild(this.parentNode);
-		}
-	}
+	$('.content-unit .title-add').on('click', function(e){
+		let clone = $(this).parent().next();
+			clone = clone.clone();
+		$(this).parent().parent().append(clone);
+	});
 
-	// let plus = document.querySelectorAll(".details .name");
-	// for(let i = 0; i < plus.length; i++){
-	// 	let item = document.createElement("i");
-	// 	item.className = " icon-plus add";
-	// 	plus[i].appendChild(item);
-	// }
-
-	// let adds = document.querySelectorAll(".add");
-	// for(let i = 0; i < adds.length; i++){
-	// 	adds[i].onclick = function(){
-	// 		let item = document.createElement("p");
-	// 		item.innerHTML = " ";
-	// 		this.children[1].appendChild(item);
-	// 	}
-	// }
-
-}
+  function removeItem(first) {
+    if (first) {
+      $(".describe p, .award-item").css({
+        position: "relative"
+      }).append($(`<i class='icon-remove remove'></i>`));
+      $(".remove").on("click", function() {
+        this.parentNode.parentNode.removeChild(this.parentNode);
+      });
+    } else {
+      if ($(".newP").find("i")) {
+        $(".newP").find("i").remove();
+      }
+      $(".newP").append($(`<i class='icon-remove newRemove'></i>`));
+      $(".newRemove").off("click");
+      $(".newRemove").on("click", function(e) {
+        this.parentNode.parentNode.removeChild(this.parentNode);
+      });
+    }
+  }
+});
